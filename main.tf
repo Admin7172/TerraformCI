1117fca9b6d34f7a973b8c13535aaf7d531e4f42
admin7172 <admin@7172company.com>
2026-04-07 13:38:11 -0500

    changes

--- main.tf
+++ main.tf
@@ -88,7 +88,7 @@ resource "azurerm_network_security_group" "nsg" {
     protocol                   = "Tcp"
     source_port_range          = "*"
     destination_port_range     = "3389"
-    source_address_prefix      = azurem_subnet.subnet_2.address_prefixes
+    source_address_prefix      = azurem_subnet.subnet_2.address_prefixes[0]
     destination_address_prefix = "*"
   }
 
@@ -129,7 +129,7 @@ resource "azurerm_subnet_network_security_group_association" "nsg_assoc" {
 # ============================================================
 
 resource "azurerm_public_ip" "pip" {
-  name                = "pip-${azurerm_bastion_host.bastion.name}"
+  name                = "pip-${var.bastion_name}"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   allocation_method   = "Static"
